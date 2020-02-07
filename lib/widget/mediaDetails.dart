2080db24b9103ad1698ed40d7654eadaf5e6b550
yeison aristizabal <kaos34k@gmail.com>
2020-02-06 23:15:54 -0500

    mejoras en detalle peliculas

--- lib/widget/mediaDetails.dart
+++ lib/widget/mediaDetails.dart
@@ -16,7 +16,26 @@ class MediaDetails extends StatelessWidget {
           Image.network(media.getPosterLarge(), fit: BoxFit.cover),
           BackdropFilter(
               filter: ui.ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
-              child: Container(color: Colors.black.withOpacity(0.5)))
+              child: Container(color: Colors.black.withOpacity(0.5))),
+              SingleChildScrollView(
+                child: Container(
+                margin: EdgeInsets.all(30.0),
+                child: Column(children: <Widget>[
+                  Container(alignment: Alignment.center,
+                  child: Container(width: 390.0, height: 390.0,),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0), 
+                    image: DecorationImage(
+                      image: NetworkImage(media.getPosterLarge())
+                    ),
+                    boxShadow: [BoxShadow(
+                      color: Colors.black,
+                      blurRadius: 20.0,
+                      offset: Offset(0.0, 10.0)
+                    )]    
+                    ),)
+                ],),
+              ))
         ],
       ),
     );
